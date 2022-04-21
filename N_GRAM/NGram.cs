d85c1c1cb81e65982875512b850a3a2855932ed9
Fan Jing <jing1999fan@outlook.com>
2022-04-21 02:43:09 -0700

    Add project files.

---
+++ N_GRAM/NGram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace NGRAM
+{
+    class NGram
+    {
+        public String os; //original sentence
+        public int n;  //value of n in N-Gram
+
+        public List<String> ngrams(String os, int n)
+        {
+            List<String> ngrams = new  List<String>();
+            String[] word = Regex.Split(os, @" ");
+            for (int i = 0; i < word.Length - n + 1; i++)
+            {
+                ngrams.Add(after(word, i, i + n));
+            }
+            return ngrams;
+        }
+        public static String after(String[] words, int start, int end)
+        {
+            StringBuilder StringB = new StringBuilder();
+            for (int i = start; i < end; i++)
+            {
+                StringB.Append((i > start ? " " : "") + words[i]);
+            }
+            return StringB.ToString();
+        }
+
+        //fail part
+       /* public String after(String[] words, int start, int end)
+        {
+            StringBuilder StringB = new StringBuilder();
+
+            for (int i = start; i < end; i++)
+            {
+                StringB.Append((i > start ? " " : "") + words[i]);
+                int count = 1;
+                for (int j = 0 ; j < end-start-1 ; j++)
+                {
+                    count++;
+                    StringB.Append(" "+ words[j]);
+                    if (count == n)
+                        continue;
+                }
+                //StringB.Append((i > start ? " " : "") + words[i]);
+            }
+            return StringB.ToString();
+        }*/
+    }
+}
---
+++ N_GRAM/N_GRAM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using NGRAM;
+
+namespace N_GRAM
+{
+    class N_GRAM
+    {
+        static void Main(string[] args)
+        {
+            NGram NG = new NGram();
+
+            Console.WriteLine("original sentence:");
+            NG.os = Console.ReadLine();
+            Console.WriteLine("value of n:");
+            NG.n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nFrom 1-gram to n-gram:\n");
+            if (NG.n>=1 && NG.n<=NG.os.Length )
+            {
+                for (int n=1;n<=NG.n;n++)
+                {
+                    Console.WriteLine("[{0}-gram]", n);
+                    foreach (String ngram in NG.ngrams(NG.os,n))
+                    {
+                        Console.WriteLine(ngram);
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
